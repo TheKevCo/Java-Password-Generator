641cf567f20579142aa7b38d55c68dc99d85ba2d
Kevin Colon <koak1995@gmail.com>
2023-01-26 01:31:32 -0500

    Correction on input for functionality

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        PasswordGen pass = new PasswordGen(5, 10,3);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many numbers would you like in password:");
+        int nums = sc.nextInt();
+        System.out.println("How many letters would you like in password:");
+        int letters = sc.nextInt();
+        System.out.println("How many symbols would you like in password:");
+        int symbols = sc.nextInt();
+
+        PasswordGen pass = new PasswordGen(nums, letters,symbols);
         System.out.println(pass.PasswordGenerator());
     }
 }
